a8f51a6d3f6902ec0bc69260aa69d80a2a25d00a
Alina <latp4pecha@gmail.com>
2025-09-14 19:13:45 +0200

    описание изменений

---
+++ AutoTestPy.py
@@ -0,0 +1,13 @@
+
+
+
+def hello_world(name):
+    print("Hello "+name+"!")
+# -*- coding: utf-8 -*- 
+
+hello_world(name='Alina')
+
+def calculate_sum(a, b):
+    return a + b
+
+print(calculate_sum(3, 5) )
