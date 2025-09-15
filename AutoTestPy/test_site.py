8aa7496cacb91253e60998a8508f6e2be337afb9
Alina <latp4pecha@gmail.com>
2025-09-15 18:18:40 +0200

    delet DS

--- AutoTestPy/test_site.py
+++ AutoTestPy/test_site.py
@@ -16,5 +16,5 @@ def test_open_s6(driver: None):
     driver.get("https://www.demoblaze.com/index.html")
     galaxy_s6 = driver.find_element(By.LinkText, value="Samsung galaxy s6")
     galaxy_s6.click()
-    title = driver.find_element(By.TagName, value="h2").text
+    title = driver.find_element(By.TagName, value="h2").text 
     assert title == "Samsung galaxy s6"
